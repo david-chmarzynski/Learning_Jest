216f1bc19efcf01e02441d35039bd69f5388b314
David Chmarzynski <david.chm00@gmail.com>
2021-01-11 12:03:48 +0100

    Async test with done

--- test/src/math.js
+++ test/src/math.js
@@ -1,3 +1,16 @@
 export function add(a, b) {
   return a + b;
 };
+
+export function fakeGetUser(user) {
+  return {
+    name: "Test",
+    age: "25"
+  };
+};
+
+export function asyncAdd(a, b, cb) {
+  setTimeout(() => {
+    cb(a + b);
+  }, 1000)
+};
--- test/src/math.test.js
+++ test/src/math.test.js
@@ -1,4 +1,4 @@
-import { add } from './math';
+import { add, fakeGetUser, asyncAdd } from './math';
 
 test("1 + 1 should return 2", () => {
   expect(add(1,1)).toBe(2);
@@ -9,3 +9,23 @@ test('-1 + -1 should return -2', () => {
 });
 
 test.todo("Write a todo test, with one positive and one negative number");
+
+test("fakeGetUser should return name Test and age 25 from object", () => {
+  const user = {
+    name: "Test",
+    age: "25"
+  };
+  expect(fakeGetUser(user)).toEqual({
+    name: "Test",
+    age: "25"
+  });
+});
+
+
+test("Async function", (done) => {
+  const cb = (res) => {
+    expect(res).toBe(3);
+    done();
+  };
+  asyncAdd(1, 2, cb);
+});
