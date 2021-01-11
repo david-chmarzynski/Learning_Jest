78a4314dce72ab708c08a1978c3ea718139c7f1e
David Chmarzynski <david.chm00@gmail.com>
2021-01-11 11:25:41 +0100

    Try test.todo

--- test/src/math.test.js
+++ test/src/math.test.js
@@ -3,3 +3,9 @@ import { add } from './math';
 test("1 + 1 should return 2", () => {
   expect(add(1,1)).toBe(2);
 });
+
+test('-1 + -1 should return -2', () => {
+  expect(add(-1, -1)).toBe(-2);
+});
+
+test.todo("Write a todo test, with one positive and one negative number");
